91e26c989e6349b126e923897137b7e4989d3aa8
jaber4770 <jaber.ahmed4770@gmail.com>
2022-02-12 18:47:54 +0600

    nav added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Home from './Components/Home/Home/Home';
+import Footer from './Components/Shared/Footer/Footer';
+import Header from './Components/Shared/Header/Header';
 
 function App() {
   return (
     <div className="App">
-      
+      <Header></Header>
+      <Home></Home>
+      <Footer></Footer>
     </div>
   );
 }
---
+++ src/Components/Home/Home/Home.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div>
+            <h1>all home component will come here</h1>
+        </div>
+    );
+};
+
+export default Home;
