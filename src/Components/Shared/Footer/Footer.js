2bc95c97d626708f343468d6e4e865b713826180
jaber4770 <jaber.ahmed4770@gmail.com>
2022-02-12 18:57:20 +0600

    all footer text added

--- src/Components/Shared/Footer/Footer.js
+++ src/Components/Shared/Footer/Footer.js
@@ -3,7 +3,42 @@ import React from 'react';
 const Footer = () => {
     return (
         <div className='bg-primary'>
-            <h1>footer</h1>
+            <div>
+                <h3>Contact info</h3>
+                <p>Phone: +88016XXXXXXXX</p>
+                <p>Email: MedicPlus@gmail.com</p>
+            </div>
+            <div>
+                <h3>Social</h3>
+                <div>
+                    <a href="https://facebook.com">
+
+                    </a>
+                    <a href="https://twitter.com">
+
+                    </a>
+                    <a href="https://youtube.com">
+
+                    </a>
+                    <a href="https://linkedin.com">
+
+                    </a>
+                </div>
+            </div>
+            <div>
+                <h3>Our Location</h3>
+                <p>location:Brahmanbaria Sadar</p>
+            </div>
+            <div>
+                <h3>Our Services</h3>
+                <ul>
+                    <li>a</li>
+                    <li>b</li>
+                    <li>c</li>
+                    <li>d</li>
+                    <li>e</li>
+                </ul>
+            </div>
         </div>
     );
 };
