3d41a054527fd1286789f6daaeb5f7a21804a8f7
jaber4770 <jaber.ahmed4770@gmail.com>
2022-02-12 19:40:29 +0600

    navbar added

--- src/Components/Shared/Header/Header.js
+++ src/Components/Shared/Header/Header.js
@@ -1,19 +1,28 @@
 import React from 'react';
+import "./Header.css"
 import { Container, Nav, Navbar } from 'react-bootstrap';
 
 const Header = () => {
     return (
         <>
-            <Navbar bg="primary" variant="dark">
+            <Navbar className='bg-clr'>
                 <Container>
                     <Navbar.Brand href="#home">
-                        <img src="https://i.ibb.co/xjfw99d/free-downloads-medical-clipart-640290.png" alt="" />
+                        <img className='navIcon' src="https://i.ibb.co/xjfw99d/free-downloads-medical-clipart-640290.png" alt="" />
+                        Medic Plus
                     </Navbar.Brand>
-                    <Nav className="me-auto">
+                    <Navbar.Toggle />
+                    <Navbar.Collapse className="justify-content-end">
                         <Nav.Link href="#home">Home</Nav.Link>
-                        <Nav.Link href="#features">Features</Nav.Link>
-                        <Nav.Link href="#pricing">Pricing</Nav.Link>
-                    </Nav>
+                        <Nav.Link href="#home">Services</Nav.Link>
+                        <Nav.Link href="#home">Appointment</Nav.Link>
+                        <Nav.Link href="#home">Emergency</Nav.Link>
+                        <Nav.Link href="#home">About</Nav.Link>
+                        <Nav.Link href="#home">Login</Nav.Link>
+                        <Navbar.Text>
+                            Signed in as: <a href="#login">sijan</a>
+                        </Navbar.Text>
+                    </Navbar.Collapse>
                 </Container>
             </Navbar>
         </>
