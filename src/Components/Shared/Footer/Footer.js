ff3030f1952ca6ec59eb173a7a3b69c8c3a99b78
jaber4770 <jaber.ahmed4770@gmail.com>
2022-02-14 23:06:34 +0600

    nav done

--- src/Components/Shared/Footer/Footer.js
+++ src/Components/Shared/Footer/Footer.js
@@ -1,38 +1,37 @@
-import { FaPhoneAlt, FaRegEnvelope, FaFacebook, FaYoutube, FaLinkedin, FaTwitter } from "react-icons/fa";
+import { FaPhoneAlt, FaRegEnvelope, FaFacebook, FaYoutube, FaLinkedin, FaTwitter, FaMapMarkerAlt } from "react-icons/fa";
 import React from 'react';
 import { Container } from 'react-bootstrap';
-import { Link } from "react-router-dom";
+import './Footer.css'
 
 const Footer = () => {
     return (
-        <div className='border'>
+        <div className="footer-top">
             <Container>
                 <div className='d-flex'>
-                    <div className="pe-5 pt-3">
+                    <div className="pe-3 pt-3">
                         <h3>Contact info</h3>
                         <p><FaPhoneAlt /> Phone: +88016XXXXXXXX</p>
                         <p><FaRegEnvelope /> Email: info@medicplus.com</p>
                     </div>
-                    <div className="pe-5 pt-3">
+                    <div className="pe-3 pt-3">
                         <h3>For Appointment</h3>
                         <p><FaPhoneAlt /> Phone: +88016XXXXXXXX</p>
-                        <p><FaRegEnvelope /> Email: MedicPlus@services.com</p>
+                        <p><FaRegEnvelope /> Email: MedicPlus@appointment.com</p>
                     </div>
-                    <div className="pe-5 pt-3">
+                    <div className="pe-3 pt-3">
                         <h3>Social</h3>
-                        <div className='text-danger'>
+                        <div className='text-danger social-icon'>
                             <a target="_blank" href="https://facebook.com"><FaFacebook /></a>
                             <a target="_blank" href="https://twitter.com"><FaTwitter /></a>
                             <a target="_blank" href="https://youtube.com"><FaYoutube /></a>
                             <a target="_blank" href="https://linkedin.com"><FaLinkedin /></a>
-                            <Link target={"_blank"} to="https://facebook.com"><FaFacebook /></Link>
                         </div>
                     </div>
-                    <div className="pe-5 pt-3">
+                    <div className="pe-3 pt-3">
                         <h3>Our Location</h3>
-                        <p>location:Brahmanbaria Sadar</p>
+                        <p><FaMapMarkerAlt />location:Brahmanbaria Sadar</p>
                     </div>
-                    <div className="pe-5 pt-3">
+                    <div className="pe-3 pt-3">
                         <h3>Our Services</h3>
                         <ul>
                             <li>a</li>
