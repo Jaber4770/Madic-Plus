2e3f9b843bb807d1ce6c1357a3ccab1a217baf49
jaber4770 <jaber.ahmed4770@gmail.com>
2022-02-19 23:21:05 +0600

    finally solve the proble

--- src/Components/Home/Service/Service.js
+++ src/Components/Home/Service/Service.js
@@ -3,13 +3,22 @@ import { Button, Card } from 'react-bootstrap';
 import { GiKidneys, GiBrain, GiBrokenBone } from "react-icons/gi";
 import { BiBone } from "react-icons/bi";
 import { MdPregnantWoman } from "react-icons/md";
+import "./Service.css"
 
 
 const Service = ({service}) => {
-    const [name, description, image] = service;
+    const {name, description, image} = service;
     return (
         <div className='border'>
-            <h1>hellow world</h1>
+            <Card style={{ width: '18rem' }}>
+                <Card.Img variant="top" src={image} />
+                <h1>{image}</h1>
+                <Card.Body>
+                    <Card.Title>{name}</Card.Title>
+                    <Card.Text>{description}</Card.Text>
+                    <Button variant="primary">Service Details</Button>
+                </Card.Body>
+            </Card>
         </div>
     );
 };
