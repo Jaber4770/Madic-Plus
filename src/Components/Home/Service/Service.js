ea0e9ebff47c4edb75cde5d9660c6fc72d816c8d
jaber4770 <jaber.ahmed4770@gmail.com>
2022-02-23 00:00:36 +0600

    modifyed

--- src/Components/Home/Service/Service.js
+++ src/Components/Home/Service/Service.js
@@ -1,20 +1,14 @@
 import React from 'react';
 import { Button, Card, CardGroup } from 'react-bootstrap';
-import { FaTooth } from "react-icons/fa";
-import { FaAccessibleIcon, MaterialDesign } from "react-icons/fa";
 
 
 
 const Service = ({ service }) => {
     const { name, description, icon } = service;
-    const mdIcon = MaterialDesign[icon];
     return (
         <div>
-            <StyledTabs icon={icon}> </StyledTabs>
-
             <Card style={{ width: '18rem' }}>
                 <Card.Img variant="top" src={icon} />
-                <FaAccessibleIcon />
                 <Card.Body>
                     <Card.Title>{icon}</Card.Title>
                     {icon}
