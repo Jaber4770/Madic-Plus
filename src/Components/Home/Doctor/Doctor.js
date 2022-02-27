f00a08c6710c3bda3d72bf64c0ef1de52e144ef6
jaber4770 <jaber.ahmed4770@gmail.com>
2022-02-27 22:09:02 +0600

    react slick slider installed

--- src/Components/Home/Doctor/Doctor.js
+++ src/Components/Home/Doctor/Doctor.js
@@ -1,10 +1,21 @@
 import React from 'react';
+import { Card } from 'react-bootstrap';
 
-const Doctor = () => {
+const Doctor = ({ DoctorData }) => {
+    const { name, special, img } = DoctorData;
     return (
         <div>
-            <h1>single doctor</h1>
+            <Card style={{ width: '18rem' }}>
+                <img src={img} alt="" />
+                <Card.Body>
+                    <Card.Title>{name}</Card.Title>
+                    <Card.Text>
+                        <p>Specialize in: {special}</p>
+                    </Card.Text>
+                </Card.Body>
+            </Card>
         </div>
+        
     );
 };
 
--- src/Components/Home/OurDoctors/OurDoctors.js
+++ src/Components/Home/OurDoctors/OurDoctors.js
@@ -1,23 +1,29 @@
 import React, { useEffect, useState } from 'react';
+import Doctor from '../Doctor/Doctor';
 import "./OurDoctors.css"
 
 
 const OurDoctors = () => {
-    const [Doctor, setDoctor] = useState([]);
+    const [DoctorData, setDoctorData] = useState([]);
 
-    useEffect( ()=> {
+    useEffect(() => {
         fetch('https://jaber4770.github.io/Medic-Plus-Services-Data/DoctorsData.json')
             .then(res => res.json())
-            .then(data => setDoctor(data))
-    },[])
+            .then(data => setDoctorData(data))
+    }, [])
 
-    
+
     return (
         <div>
             <h1>Meet Our Doctors</h1>
             <p>Everyone is an expert in their field.</p>
-            <div>
-
+            <div className='card-position'>
+                {
+                    DoctorData.map(data => <Doctor
+                        key={data.id}
+                        DoctorData={data}
+                    ></Doctor>)
+                }
             </div>
         </div>
     );
