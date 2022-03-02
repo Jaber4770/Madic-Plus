c2a10657001bd8cf9d3ed43cb78e0fd9623baaec
jaber4770 <jaber.ahmed4770@gmail.com>
2022-03-03 00:27:17 +0600

    modified

--- src/Components/Home/Appointment/AppointmentForm.js
+++ src/Components/Home/Appointment/AppointmentForm.js
@@ -6,15 +6,22 @@ const AppointmentForm = () => {
     const onSubmit = data => console.log(data);
     return (
         <div>
-            <form onSubmit={handleSubmit(onSubmit)}>
-                <input defaultValue="Name" {...register("Name", { required: true })} />
-                <input defaultValue="Phone" {...register("Phone", { required: true })} />
-                <input defaultValue="Appointment Date" {...register("Appointment-Date", { required: true })} />
-                <input defaultValue="Appointment Time" {...register("Appointment-Time", { required: true })} />
-                {/* errors will return when field validation fails  */}
-                {errors.exampleRequired && <span>This field is required</span>}
-                <input type="submit" />
-            </form>
+            <h1 className='text-center mt-5 mb-4'>Appointment</h1>
+            <div className='d-flex justify-content-center align-items-center'>
+                <div>
+                    <img src="http://asianitbd.com/wp/hopemedical/wp-content/uploads/2016/10/image-forlift.png" alt="" />
+                </div>
+                <form onSubmit={handleSubmit(onSubmit)}>
+                    <input className='appointment-name' defaultValue="Name" {...register("Name", { required: true })} />
+                    <input className='appointment-phone' defaultValue="Phone" {...register("Phone", { required: true })} />
+                    <input className='appointment-date' defaultValue="Appointment Date" {...register("Appointment-Date", { required: true })} />
+                    <input className='appointment-time' defaultValue="Appointment Time" {...register("Appointment-Time", { required: true })} />
+                    <input className='appointment-problem' defaultValue="Problem" {...register("Problem", { required: true })} />
+                    {/* errors will return when field validation fails  */}
+                    {errors.exampleRequired && <span>This field is required</span>}
+                    <input type="submit" />
+                </form>
+            </div>
         </div>
     );
 };
