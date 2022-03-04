bd8d06b8384b7dc181e0a52b915342ae3138bd83
jaber4770 <jaber.ahmed4770@gmail.com>
2022-03-04 19:56:53 +0600

    modified form

--- src/Components/Home/Appointment/AppointmentForm.js
+++ src/Components/Home/Appointment/AppointmentForm.js
@@ -13,12 +13,12 @@ const AppointmentForm = () => {
                     <img src="http://asianitbd.com/wp/hopemedical/wp-content/uploads/2016/10/image-forlift.png" alt="" />
                 </div>
                 <form className='form' onSubmit={handleSubmit(onSubmit)}>
-                    <input className='appointment appointment-name' defaultValue="Name" {...register("Name", { required: true })} />
-                    <input className='appointment appointment-phone' defaultValue="Phone" {...register("Phone", { required: true })} />
+                    <input placeholder='Name' className='appointment appointment-name' {...register("Name", { required: true })} />
+                    <input placeholder='Phone' className='appointment appointment-phone' {...register("Phone", { required: true })} /> <br />
                     <input className='appointment appointment-date' 
-                    type="date" {...register("Appointment-Date", { required: true })} />
-                    <input className='appointment appointment-time' defaultValue="Appointment Time" {...register("Appointment-Time", { required: true })} />
-                    <input className='appointment appointment-problem' defaultValue="Problem" {...register("Problem", { required: true })} />
+                    type="date" {...register("Appointment-Date", { required: true })} /> <br />
+                    <input className='appointment appointment-time' type="time" {...register("Appointment-Time", { required: true })} /> <br />
+                    <input placeholder='Your problem' width="300" height="100" className='appointment appointment-problem' {...register("Problem", { required: true })} />
                     {/* errors will return when field validation fails  */}
                     {errors.exampleRequired && <span>This field is required</span>}
                     <input className='appointment' type="submit" />
