89acee4a10ab1b666884c01cfbf4a15c1816ddb6
jaber4770 <jaber.ahmed4770@gmail.com>
2022-03-06 21:51:23 +0600

    services card working

--- src/Components/Service/Service.js
+++ src/Components/Service/Service.js
@@ -1,9 +1,22 @@
 import React from 'react';
+import { Button, Card } from 'react-bootstrap';
 
-const Service = () => {
+const Service = ({ service }) => {
+    const { name, img, description } = service
     return (
         <div>
-            <h1>service</h1>
+            <Card className='card-hover' style={{ width: '20rem', height: '100%' }}>
+                <Card.Img variant="top" src={img} />
+                <Card.Body>
+                    <Card.Title><h2>{name}</h2></Card.Title>
+                    <Card.Text>
+                        <p className='pera-center'>{description}</p>
+                    </Card.Text>
+                    <div className='card-footer-btm'>
+                        <Button className='btn-hover' variant="primary">Details</Button>
+                    </div>
+                </Card.Body>
+            </Card>
         </div>
     );
 };
