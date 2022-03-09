e6ea29f2a1924eebdb821e607bdb99f494f05fa3
jaber4770 <jaber.ahmed4770@gmail.com>
2022-03-09 23:11:57 +0600

    form added

--- src/Components/Appointment/Appointment.js
+++ src/Components/Appointment/Appointment.js
@@ -7,25 +7,35 @@ const Appointment = () => {
     const { register, handleSubmit } = useForm();
     const [data, setData] = useState("");
     return (
-        <div className='my-5'>
+        <div className='py-5 form-bg'>
             <Container>
                 <div className='appointment-heading'>
                     <h1 className='text-center mb-3'>Take your <span className='span-color span-underline'>Appointment</span></h1>
                 </div>
-                <form onSubmit={handleSubmit((data) => setData(JSON.stringify(data)))}>
-                    <label htmlFor="Name">Name: </label>
-                    <input className='input ' {...register("Name")} placeholder="Name" />
-                    <label htmlFor="Phone">Phone: </label>
-                    <input className='input ' {...register("Phone")} placeholder="Phone" /><br />
-                    <label htmlFor="Email">Email: </label>
-                    <input className='input ' {...register("Email")} placeholder="Email" /><br />
-                    <label htmlFor="Address">Address: </label>
-                    <input className='input ' {...register("Address")} placeholder="Address" /><br />
-                    <label htmlFor="problem">Your Problem: </label>      
-                    <textarea className='input ' {...register("Describe your problem")} placeholder="About you" />
-                    <p>{data}</p>
-                    <input type="submit" />
-                </form>
+                <div className='form-center'>
+                    <form onSubmit={handleSubmit((data) => setData(JSON.stringify(data)))}>
+                        <label htmlFor="Name">Name: </label><br />
+                        <input className='input ' {...register("Name")} placeholder="Name" />
+                        <br />
+                        <br />
+                        <label htmlFor="Phone">Phone: </label><br />
+                        <input className='input ' {...register("Phone")} placeholder="Phone" />
+                        <br />
+                        <br />
+                        <label htmlFor="Email">Email: </label><br />
+                        <input className='input ' {...register("Email")} placeholder="Email" />
+                        <br />
+                        <br />
+                        <label htmlFor="Address">Address: </label><br />
+                        <input className='input ' {...register("Address")} placeholder="Address" />
+                        <br />
+                        <br />
+                        <label htmlFor="problem">Your Problem: </label><br />
+                        <textarea className='input problem-box' {...register("Problem")} placeholder="About you" />
+                        <p>{data}</p>
+                        <input type="submit" />
+                    </form>
+                </div>
             </Container>
         </div>
     );
