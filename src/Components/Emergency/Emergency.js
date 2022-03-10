54bdff4705c281aaf7d063640f9601a6f535d05e
jaber4770 <jaber.ahmed4770@gmail.com>
2022-03-10 14:58:40 +0600

    making emergency page

--- src/Components/Emergency/Emergency.js
+++ src/Components/Emergency/Emergency.js
@@ -5,8 +5,12 @@ import "./Emergency.css";
 const Emergency = () => {
     return (
         <div>
+            <img className='amblance-banner-img' src="https://i.ibb.co/HdKn2Hp/ambulance-banner.webp" alt="" />
             <Container>
-                <h1>we are ready for emergency services</h1>
+                <h1 className='my-3 text-center'>24/7 We are ready for your call!</h1>
+                <div>
+                    <img src="https://i.ibb.co/5Y20BkS/ambulance-concept-illustration-114360-7051.jpg" alt="" />
+                </div>
             </Container>
         </div>
     );
