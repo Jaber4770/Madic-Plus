a50a260c71fbfdfee43273304dc08239ad3a96da
jaber4770 <jaber.ahmed4770@gmail.com>
2022-03-12 22:36:37 +0600

    our doctors box shadow added

--- src/Components/Home/Doctor/Doctor.js
+++ src/Components/Home/Doctor/Doctor.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { Card } from 'react-bootstrap';
+import "./Doctors.css"
 
 const Doctor = ({ DoctorData }) => {
     const { name, special, img } = DoctorData;
     return (
         <div>
-            <Card style={{ width: '18rem' }}>
+            <Card className='doctors-card-hover' style={{ width: '18rem' }}>
                 <img src={img} alt="" />
                 <Card.Body>
                     <Card.Title>{name}</Card.Title>
