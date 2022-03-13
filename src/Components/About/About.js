1d92a76c53e1ac4f2cc952cad4817a1b5079e032
jaber4770 <jaber.ahmed4770@gmail.com>
2022-03-13 12:55:19 +0600

    hstory text added

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -18,37 +18,31 @@ const About = () => {
                     <div className='text-center my-4 history-underline'>
                         <h2><span className='span-color'>Medica Plus</span> History<span className='span-color'>!</span></h2>
                         <h3>EVERYTHING BEGAN IN A <span className='span-color'>GARAGE.</span></h3>
-                        <p>Pellentesque semper quis neque dictum hendrerit. Sed nulla ipsum, porttitor pharetra tortor in, <br></br>
-                            consequat imperdiet nisi. Phasellus at quam tristique, cursus tellus vitae, convallis neque.</p>
+                        <p className='history-pera'>Aliquam magna nibh, mattis a urna nec, semper venenatis magna. Cras suscipit neque libero, <br /> in bibendum orci scelerisque mollis. Nunc auctor ut erat vel euismod. Aenean tristique leo eros, at gravida massa semper eu.</p>
                     </div>
                 </div>
                 <div>
                     <div className='maing-text'>
                         <div className='history-div'>
-                            <h1 className='our-history-underline'>Our <span className='span-color'>Stories</span></h1>
+                            <h1 className='our-history-underline mt-3 mb-4'>Our <span className='span-color'>Stories</span></h1>
                         </div>
                         <div>
-                            <h3>Back in 1982</h3>
-                            <p>Aliquam magna nibh, mattis a urna nec, semper venenatis magna. Cras suscipit neque libero, in bibendum orci scelerisque mollis. Nunc auctor ut erat vel euismod. Aenean tristique leo eros, at gravida massa semper eu.
-
-                                Fusce imperdiet libero et eros tincidunt, vitae accumsan purus pulvinar. Vivamus varius, metus et scelerisque viverra. Morbi mattis, lorem sit amet gravida eleifend, nisl felis tincidunt dui, nec porta turpis ligula eu nisl. Donec lacus dolor, facilisis at tortor ac, congue vulputate.</p>
+                            <h4>Back in 1982</h4>
+                            <p className='history-pera'>Aliquam magna nibh, mattis a urna nec, semper venenatis magna. Cras suscipit neque libero, in bibendum orci scelerisque mollis. Nunc auctor ut erat vel euismod. Aenean tristique leo eros, at gravida massa semper eu.</p>
+                            <p className='history-pera'>Fusce imperdiet libero et eros tincidunt, vitae accumsan purus pulvinar. Vivamus varius, metus et scelerisque viverra. Morbi mattis, lorem sit amet gravida eleifend, nisl felis tincidunt dui, nec porta turpis ligula eu nisl. Donec lacus dolor, facilisis at tortor ac, congue vulputate.</p>
                         </div>
                         <div>
-                            <h3>The tipping point</h3>
-                            <p>Quisque eleifend volutpat orci nec fermentum. Curabitur quis dignissim nibh. Suspendisse potenti. Vivamus dictum volutpat dapibus. Fusce sed varius massa. Fusce et purus sit amet erat fermentum rutrum. Pellentesque pellentesque auctor vehicula. Integer ut ligula blandit, pellentesque neque in, hendrerit magna.
-
-                                Nunc porta vehicula mauris, a hendrerit dolor tempor sed. Etiam euismod dignissim fermentum. Maecenas tincidunt finibus auctor.
-
-                                Proin nec tempus nulla, et euismod nulla. Donec ut sem ipsum. Integer ultricies nisi sed augue interdum porta. Vestibulum ac aliquam risus, ac dictum magna. Fusce lacus arcu, facilisis eu odio nec, varius vestibulum sem.
-
-                                Nulla venenatis auctor quam, ac porta lectus dictum in
-                                Aliquam sodales nisi sit amet lorem
-                                Suspendisse vulputate tellus in justo convallis imperdiet
-                                Morbi id lorem consequat, sodales mi non
-                                Cras fringilla efficitur elementum
-                                Nulla venenatis auctor quam, ac porta lectus dictum in
-                                Aliquam sodales nisi sit amet lorem
-                                Aliquam sodales facilisis dolor, feugiat elementum tellus condimentum at. Mauris sem nisi, dictum eget facilisis quis, placerat eu nisl. Phasellus consectetur pharetra elit id maximus. Aliquam facilisis sodales erat, sit amet euismod eros dignissim aliquam.</p>
+                            <h4>The tipping point</h4>
+                            <p className='history-pera'>Proin nec tempus nulla, et euismod nulla. Donec ut sem ipsum. Integer ultricies nisi sed augue interdum porta. Vestibulum ac aliquam risus, ac dictum magna. Fusce lacus arcu, facilisis eu odio nec, varius vestibulum sem.</p>
+                            <p className='history-pera'>Nulla venenatis auctor quam, ac porta lectus dictum in
+                                <p className='ms-4 history-pera'>
+                                    <br />Aliquam sodales nisi sit amet lorem
+                                    <br />Suspendisse vulputate tellus in justo <br />convallis imperdiet
+                                    <br />Morbi id lorem consequat, sodales mi non
+                                    <br />Cras fringilla efficitur elementum
+                                    <br />Nulla venenatis auctor quam, ac porta <br />lectus dictum in
+                                    <br />Aliquam sodales nisi sit amet lorem</p></p>
+                            <p className='history-pera'>Aliquam sodales facilisis dolor, feugiat elementum tellus condimentum at. Mauris sem nisi, dictum eget facilisis quis, placerat eu nisl. Phasellus consectetur pharetra elit id maximus. Aliquam facilisis sodales erat, sit amet euismod eros dignissim aliquam.</p>
                         </div>
                     </div>
                     <div className='side-bar'>
