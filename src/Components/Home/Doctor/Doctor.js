0bc2ef06f59edff3510649a92c7dd55ccf6340aa
jaber4770 <jaber.ahmed4770@gmail.com>
2022-03-14 22:15:01 +0600

    our doctors card animation added done

--- src/Components/Home/Doctor/Doctor.js
+++ src/Components/Home/Doctor/Doctor.js
@@ -6,15 +6,17 @@ const Doctor = ({ DoctorData }) => {
     const { name, special, img } = DoctorData;
     return (
         <div>
-            <Card className='doctors-card-hover' style={{ width: '18rem' }}>
-                <img src={img} alt="" />
-                <Card.Body>
-                    <Card.Title>{name}</Card.Title>
-                    <Card.Text>
-                        <p>Specialize in: {special}</p>
-                    </Card.Text>
-                </Card.Body>
-            </Card>
+            <div data-aos="fade-up" data-aos-duration="3000">
+                <Card className='doctors-card-hover' style={{ width: '18rem' }}>
+                    <img src={img} alt="" />
+                    <Card.Body>
+                        <Card.Title>{name}</Card.Title>
+                        <Card.Text>
+                            <p>Specialize in: {special}</p>
+                        </Card.Text>
+                    </Card.Body>
+                </Card>
+            </div>
         </div>
 
     );
