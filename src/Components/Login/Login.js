4ae20b1a580430a8c006f00ac68cbd151fd6d5ca
jaber4770 <jaber.ahmed4770@gmail.com>
2022-03-15 11:04:49 +0600

    login image added

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -1,12 +1,22 @@
 import React from 'react';
 import { Container } from 'react-bootstrap';
+import './Login.css'
 
 const Login = () => {
     return (
         <div>
             <Container>
-                <h1>Please Login!</h1>
-                
+                <h1 className='text-center my-4'>Please Login!</h1>
+                <div className='log-d-flex'>
+                    <div className='login-half-width login-img'>
+                        <img className='log-img-w' src="https://i.ibb.co/HpkKyHd/4957136.jpg" alt="" />
+                    </div>
+                    <div className='login-bg login-half-width'>
+                        <div>
+                            <img src="" alt="" />
+                        </div>
+                    </div>
+                </div>
             </Container>
         </div>
     );
