f53578621c21f921ca534832fb449d8e418da3a2
jaber4770 <jaber.ahmed4770@gmail.com>
2022-04-15 11:57:37 +0600

    commented

--- src/Hooks/useAuth.js
+++ src/Hooks/useAuth.js
@@ -1,6 +1,8 @@
 import { useContext } from 'react';
 import { AuthContext } from '../Components/ContextApi/AuthProvider';
 
+// this is useAuth, it will use every where, when need
+
 const useAuth = () => {
     return useContext(AuthContext);
 };
