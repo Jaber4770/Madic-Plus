468870a89c54a175abfedaeadddc83eba9d79bb3
jaber4770 <jaber.ahmed4770@gmail.com>
2022-04-20 22:11:48 +0600

    commentted

--- src/Components/NotFound/NotFound.js
+++ src/Components/NotFound/NotFound.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import './NotFound.css'
 
+// notfund page
 const NotFound = () => {
     return (
         <div className='notfound'>
